e924469d9074596baac4afabc78cfe1668888c4f
noxbug <pieterjan.dingenen@outlook.com>
2020-01-07 20:59:10 +0000

    make print function work

--- pyprint.py
+++ pyprint.py
@@ -2,6 +2,7 @@ import os
 import re
 import email
 import imaplib
+import subprocess
 
 # %% custom exception class
 #class SenderNotTrustedError(Exception):
@@ -10,9 +11,12 @@ import imaplib
 #class NoAttachmentError(Exception):
 #    pass
 
+# %% get path of current file
+dirname = os.path.dirname(os.path.abspath(__file__))
+
 # %% login
 CREDENTIALS = {}
-with open('credentials') as fid:
+with open(os.path.join(dirname, 'credentials')) as fid:
     CREDENTIALS['HOST'] = fid.readline().rstrip()
     CREDENTIALS['USER'] = fid.readline().rstrip()
     CREDENTIALS['PASS'] = fid.readline().rstrip()
@@ -35,7 +39,7 @@ for mid in data[0].split()[-5:]:
     
     # %% validate sender
     sender = re.search(r'<(.+?)>', header.get('From')).group(1)
-    with open('whitelist') as fid:
+    with open(os.path.join(dirname, 'whitelist')) as fid:
         whitelist = fid.read()
         if sender in whitelist:
             pass
@@ -56,20 +60,26 @@ for mid in data[0].split()[-5:]:
     message = email.message_from_string(mail)
     
     # %% download email attachment
+    file_type = ['pdf', 'jpg', 'png']
     for part in message.walk():
         content_type = part.get_content_type()
-        if ('pdf' in content_type):
+        if any([x in content_type for x in file_type]):
             file_name = part.get_filename()
             path = os.path.join(os.path.expanduser('~'), 'Downloads', file_name)
             with open(path, 'wb') as fid:
                 fid.write(part.get_payload(decode=True))
+            
+            # %% print attachment
             print('Attachment saved: ' + path)
-        
-    # %% delete mail
-    imap.copy(mid,'Deleted')
-    # use uid since messages are renumbered after deletion
-    # imap.store(mid, '+FLAGS', '\\Deleted')
-    imap.uid('STORE', uid , '+FLAGS', '\\Deleted')
+            # print attachment
+            print(subprocess.check_output(['lp', path]).decode().rstrip())
+            
+            # %% delete mail
+            # use uid since messages are renumbered after deletion
+            # imap.copy(mid,'Deleted')
+            # imap.store(mid, '+FLAGS', '\\Deleted')
+            imap.uid('COPY', uid, 'Deleted')
+            imap.uid('STORE', uid , '+FLAGS', '\\Deleted')
 
 # %% close connection
 imap.expunge()            
